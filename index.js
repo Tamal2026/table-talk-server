99ffb75ae0bd5c44bc2c6dd810d2a0d5acead0cb
Tamal Krishna <tamalkrishna2026@gmail.com>
2024-11-01 17:21:15 +0600

    update the food item

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
     const UserCollection = client.db("tableTalk").collection("users");
     const menuCollection = client.db("tableTalk").collection("menu");
     const cartCollection = client.db("tableTalk").collection("carts");
-
+    // jwt related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
@@ -41,32 +41,77 @@ async function run() {
     // Middleware to verify JWT token
     const verifyToken = (req, res, next) => {
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "Forbidden Access" });
+        return res.status(401).send({ message: "Unauthorized Access" });
       }
 
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "Forbidden Access" });
+          return res.status(401).send({ message: "Unauthorized Access" });
         }
 
         req.decoded = decoded;
         next();
       });
     };
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await UserCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
 
-    // jwt related api
-
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+    // Menu Related Apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
 
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          img: item.img,
+          short_desc: item.short_desc,
+          description: item.description,
+        },
+      };
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
     // Users related APIs
-    app.get("/users", verifyToken, async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await UserCollection.find().toArray();
       res.send(result);
     });
+
     app.get("/users/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       if (email !== req.decoded.email) {
@@ -78,7 +123,7 @@ async function run() {
       if (user) {
         admin = user?.role === "admin";
       }
-      res.send({admin})
+      res.send({ admin });
     });
 
     app.patch("/users/:id", async (req, res) => {
@@ -111,7 +156,7 @@ async function run() {
       }
     });
 
-    app.delete("/users/:id", async (req, res) => {
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
 
       // Validate the ID
